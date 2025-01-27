42613a54911434795aff7665a3fc1377dcc6d8c8
Tomasz Matras <tmatras@whitelabelcoders.com>
2025-01-27 20:33:56 +0100

    Fix prettier, add fonts, set media for nav-bar, add router-link class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,30 +5,28 @@ import { OfferPageComponent } from './pages/offer-page/offer-page.component';
 import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
 import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
 
-
-
 const routes: Routes = [
-  {
-    path: '',
-    component: StartPageComponent,
-  },
-  {
-    path: 'offer',
-    component: OfferPageComponent,
-  },
-  {
-    path: 'projects',
-    component: ProjectsPageComponent,
-
-  },
-  {
-    path: 'contact',
-    component: ContactPagesComponent,
-  },
+	{
+		path: '',
+		component: StartPageComponent,
+	},
+	{
+		path: 'offer',
+		component: OfferPageComponent,
+	},
+	{
+		path: 'projects',
+		component: ProjectsPageComponent,
+	},
+	{
+		path: 'contact',
+		component: ContactPagesComponent,
+	},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
+
