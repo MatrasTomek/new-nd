f267f3f2ea738ac7e2840a533ff7c2bbd165b1ba
Tomasz Matras <matras.tomasz@outlook.com>
2025-02-11 21:58:47 +0100

    Add page-footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    PagesModule,
-    SharedModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, AppRoutingModule, PagesModule, SharedModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarModule } from './nav-bar/nav-bar.module';
-
-
-
-
+import { FooterModule } from './footer/footer.module';
 
 @NgModule({
-  declarations: [
-
-  ],
-  imports: [
-    CommonModule,
-    NavBarModule,
-
-  ],
-  exports: [NavBarModule]
+	declarations: [],
+	imports: [CommonModule, NavBarModule, FooterModule],
+	exports: [NavBarModule, FooterModule],
 })
-export class SharedModule { }
+export class SharedModule {}
