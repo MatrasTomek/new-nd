8809bf62f35e34c169a790224c18bd2b07b055da
Tomek Matras <matras.tomasz@outlook.com>
2025-12-27 10:15:34 +0100

    Add offer page content, styles, and toggle functionality

--- src/app/pages/offer-page/offer-page.component.ts
+++ src/app/pages/offer-page/offer-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-offer-page',
-    templateUrl: './offer-page.component.html',
-    styleUrls: ['./offer-page.component.scss'],
-    standalone: false
+	selector: 'app-offer-page',
+	templateUrl: './offer-page.component.html',
+	styleUrls: ['./offer-page.component.scss'],
+	standalone: false,
 })
 export class OfferPageComponent {
+	wrapperOpen: boolean = false;
 
+	toggleWrapper() {
+		this.wrapperOpen = !this.wrapperOpen;
+	}
 }
