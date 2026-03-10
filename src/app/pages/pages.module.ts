359c1dce69c2cde6c1bda98c0f307791e7eacc11
Tomek Matras <matras.tomasz@outlook.com>
2026-03-10 14:49:51 +0100

    Update offer page links to use routerLink and enhance footer navigation

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,9 +5,10 @@ import { OfferPageComponent } from './offer-page/offer-page.component';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { ContactPagesComponent } from './contact-pages/contact-pages.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
 	declarations: [StartPageComponent, OfferPageComponent, ProjectsPageComponent, ContactPagesComponent],
-	imports: [CommonModule, ReactiveFormsModule],
+	imports: [CommonModule, ReactiveFormsModule, RouterModule],
 })
 export class PagesModule {}
